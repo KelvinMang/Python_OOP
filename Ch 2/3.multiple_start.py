915c18ad26f074539f7b189694a21abbfac52dc9
Kelvin Mang <42444869+KelvinMang@users.noreply.github.com>
2022-07-26 23:19:29 +0800

    26/7/2022

--- Ch 2/3.multiple_start.py
+++ Ch 2/3.multiple_start.py
@@ -6,17 +6,25 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = "Class B"
 
 
 class C(A, B):
     def __init__(self):
         super().__init__()
 
+    def show_class(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
 
 c = C()
+c.show_class()
+print(C.__mro__)
